2d3fda9630174d3597de578280d6ab389c87b863
Marcus-Vinicius-Dev <marcus.vini.dev@gmail.com>
2023-10-02 01:43:00 -0300

    exclusão de mais arquivos

--- cadastro-django/erp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from core.models import Estudante, Instituicao, Cursos
-#from core.models import Instituicao
-
-# Register your models here.
-admin.site.register(Instituicao)
-admin.site.register(Estudante)
-admin.site.register(Cursos)

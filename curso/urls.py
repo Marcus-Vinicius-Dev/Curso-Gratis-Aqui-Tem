92669601fac97fcc460d12c9080ba4b7fa30a185
Marcus-Vinicius-Dev <marcus.vini.dev@gmail.com>
2023-10-05 18:07:26 -0300

    exclusão

--- curso/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('minha-pagina/', views.minha_pagina, name='minha_pagina'),
-]
--- curso/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def minha_pagina(request):
-    return render(request, 'curso/minha_pagina.html')

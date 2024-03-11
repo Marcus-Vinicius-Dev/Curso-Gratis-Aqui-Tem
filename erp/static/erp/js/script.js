59613f09507e0dc3187a88f97b2ed4dd7d317ba4
Marcus-Vinicius-C-S <ciapdivtectic@gmail.com>
2024-03-11 14:47:10 -0300

    correção de cabeçalho mas ainda sem rolagem infinita

--- erp/static/erp/js/script.js
+++ erp/static/erp/js/script.js
@@ -1,50 +1,28 @@
 let carregarImagens = false;
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+  const imagens = [
+      'artes.jpg',
+      'chiron.jpg',
+      'danca.jpg',
+      'esportes.jpg',
+      'idiomas.jpg',
+      'musica.jpg',
+      'negocios.jpg',
+      'relogio.jpg',
+      'relogio2.jpg',
+      'technology2.jpg',
+      'technology2_dagua.jpg',
+      'tecnologia.jpg'
+  ];
 
-function carregarMaisPostagens() {
-  console.log('Chamando carregarMaisPostagens');
-  if (
-    !carregarImagens &&
-    window.innerHeight + window.scrollY >= document.body.offsetHeight - 3
-  ) {
-    console.log('Condição de carregamento atendida');
-    carregarImagens = true;
+  const container = document.getElementById('imagens-rolagem-infinita');
 
-    document.getElementById('indicador-de-carregamento').style.display = 'block';
-
-    setTimeout(function () {
-      console.log('Timeout executado');
-      const novoContainer = document.createElement('div');
-      novoContainer.classList.add('postagens-container');
-
-      let novoPost = '';
-      const quantidadeDeImagens = 30; // Defina a quantidade de imagens a serem carregadas
-      for (let i = 1; i <= quantidadeDeImagens; i++) {
-        if (i % 2 === 0) { // Carregar apenas imagens com números pares
-          console.log(`Criando imagem ${i}`);
-          const novaImagem = document.createElement('img');
-          novaImagem.classList.add('imagem-post');
-          novaImagem.src = `/static/erp/img/imagem_postagem_${i}.jpg`;
-          novaImagem.alt = `Imagem ${i}`;
-          novoContainer.appendChild(novaImagem);
-        }
-      }
-
-      document.body.appendChild(novoContainer);
-
-      document.getElementById('indicador-de-carregamento').style.display = 'none';
-
-      carregarImagens = false;
-    }, 2000);
-  }
-}
-
-function detectorDeRolagem() {
-  window.addEventListener('scroll', function () {
-    if (
-      window.innerHeight + window.scrollY >= document.body.offsetHeight - 3
-    ) {
-      console.log('Rolagem detectada');
-      carregarMaisPostagens();
-    }
+  imagens.forEach((imagem, index) => {
+      const novaImagem = document.createElement('img');
+      novaImagem.classList.add('imagem-post');
+      novaImagem.src = `/static/erp/img/${imagem}`;
+      novaImagem.alt = `Imagem ${index}`;
+      container.appendChild(novaImagem);
   });
-}
+});
